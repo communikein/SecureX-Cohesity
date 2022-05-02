e6cce55ba55a72ac62a956e5cc5a6d4612614345
Elia Maracani <emaracan@cisco.com>
2022-05-02 13:48:30 +0200

    Fixed wrong http request method

--- bot.py
+++ bot.py
@@ -14,36 +14,30 @@ or implied.
 
 from flask import Flask, request, jsonify
 import requests
-from webexteamssdk import WebexTeamsAPI
 import os
 
 securex_url = 'https://securex-ao.eu.security.cisco.com'
 
 # get environment variables
-WT_BOT_TOKEN = os.environ['WT_BOT_TOKEN']
 WT_BOT_EMAIL = os.environ['WT_BOT_EMAIL']
 SECUREX_TOKEN = os.environ['SECUREX_TOKEN']
 SECUREX_WEBHOOK_ID = os.environ['SECUREX_WEBHOOK_ID']
 
 # start Flask and WT connection
 app = Flask(__name__)
-api = WebexTeamsAPI(access_token=WT_BOT_TOKEN)
 
 # defining the decorater and route registration for incoming alerts
 @app.route('/', methods=['POST'])
 def alert_received():
     raw_json = request.get_json()
-    print(raw_json)
-
-    # customize the behaviour of the bot here
-    message = "Hi, I am a Webex Teams bot. Have a great day ☀! "
-
-    WT_ROOM_ID = raw_json['data']['roomId']
     personEmail_json = raw_json['data']['personEmail']
+
     if personEmail_json != WT_BOT_EMAIL:
-        requests.get(securex_url + '/webhooks/' + SECUREX_WEBHOOK_ID + '?api_key=' + SECUREX_TOKEN)
-        api.messages.create(roomId=WT_ROOM_ID, markdown=message)
-    
+        args = {
+            'api_key': SECUREX_TOKEN
+        }
+        response = requests.post(securex_url + '/webhooks/' + SECUREX_WEBHOOK_ID, params=args)
+        
     return jsonify({'success': True})
 
 if __name__=="__main__":
